85bf9f95f572663ffbb923eb5999072dacd01dd4
Ken Boyer <cool.dev206@gmail.com>
2017-07-20 05:08:05 -0400

    demo

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+var cool = require('cool-ascii-faces');
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = (process.env.PORT || 8080);
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 // Listen for requests
 const server = app.listen(port, () => {
   console.log(`Magic happens on port ${server.address().port}`);
